94b4cdde5e2b7bd1f1783740e1e4a745dfdfaac7
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2010-12-01 20:50:55 +0000

    * OpenSauceIDE: Fixed up the UI to have a dark background with light green text * OpenSauceIDE: Added command line function for creating the Halo1_CE OS HEK exe's * Halo1_Installer: Initial commit of WiX installer definitions

--- BlamLib/OpenSauceIDE/Tool.cs
+++ BlamLib/OpenSauceIDE/Tool.cs
@@ -20,6 +20,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using OpenSauceIDE.CheApeInterface;
 
 namespace BlamLib
 {
@@ -30,6 +31,7 @@ namespace BlamLib
 		public static Dictionary<string, CommandFunction> kCommands = new Dictionary<string, CommandFunction>()
 		{
 			{"build-tag-database", BuildTagDatabase},
+			{"apply-cheape", UnlockBlamTools}, //apply-cheape version g_path t_path s_path output
 		};
 
 		static void BuildTagDatabase(List<string> args)
@@ -42,6 +44,56 @@ namespace BlamLib
 
 			string arg_source_tag = args[0];
 			string arg_dest_db = args[1];
+		}
+
+		static void UnlockBlamTools(List<string> args)
+		{
+			if (args.Count < 5)
+			{
+				Console.WriteLine("error: invalid command argument count");
+				return;
+			}
+
+			BlamVersion version = BlamVersion.Unknown;
+			switch (args[0])
+			{
+				case "Halo1_CE":
+					version = BlamVersion.Halo1_CE;
+					break;
+                //case "Halo2_PC":
+                //    version = BlamVersion.Halo2_PC;
+                //    break;
+				default:
+					Console.WriteLine("error: unsupported engine version - {0}", version.ToString());
+					break;
+			};
+			Console.WriteLine("Applying CheApe modifications...");
+			bool no_errors = true;
+			switch (version)
+			{
+				case BlamVersion.Halo1_CE:
+					try
+					{
+						var h1 = new UnlockH1(args[4], args[1], args[2], args[3]);
+						h1.Unlock(false);
+						h1.Close();
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine("error: an exception occurred when attempting to apply the CheApe modifications");
+						Console.WriteLine();
+						Console.WriteLine("exception details:");
+						Console.WriteLine(ex.Message);
+						Console.WriteLine();
+						no_errors = false;
+					}
+					break;
+			}
+
+			string msg = no_errors ?
+				"CheApe successfully applied!" :
+				"There was an error while trying to apply CheApe. Validate that you selected copies of the original tools and try again.";
+			Console.WriteLine(msg);
 		}
 	};
 }
