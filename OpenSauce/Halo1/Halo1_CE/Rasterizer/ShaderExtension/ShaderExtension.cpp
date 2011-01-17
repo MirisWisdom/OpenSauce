abc9a0744a69a6314b40cf55ecab6fbcbadcf662
TheFieryScythe <thefieryscythe@gmail.com>
2011-01-17 15:49:06 +0000

    * Halo1_CE: Extended ShaderExtension to allow the addition of new pixel shaders so that better performace can be achieved.

--- OpenSauce/Halo1/Halo1_CE/Rasterizer/ShaderExtension/ShaderExtension.cpp
+++ OpenSauce/Halo1/Halo1_CE/Rasterizer/ShaderExtension/ShaderExtension.cpp
@@ -83,20 +83,299 @@ namespace Yelo
 				PAD32;
 			};
 
+			typedef char t_shader_usage_id[128];
 			typedef void (PLATFORM_API *shadersetup_func)(void*, void*, void*, void*, void*, void*, void*);
 
 			static const char* g_ps_collection_format_a =	"shaders\\EffectCollection_ps_%d_a.enc";
 			static const char* g_ps_collection_format_b =	"shaders\\EffectCollection_ps_%d_b.enc";
 			static const char* g_vs_collection_path =		"shaders\\vsh_OS.enc";
+			static t_shader_usage_id g_shader_usage_id_list[48] = {
+				"MaskDetailBeforeReflectionBiasedMultiply",
+				"MaskDetailBeforeReflectionMultiply",
+				"MaskDetailBeforeReflectionBiasedAdd",
+				"MaskDetailAfterReflectionBiasedMultiply",
+				"MaskDetailAfterReflectionMultiply",
+				"MaskDetailAfterReflectionBiasedAdd",
+				"MaskInverseDetailBeforeReflectionBiasedMultiply",
+				"MaskInverseDetailBeforeReflectionMultiply",
+				"MaskInverseDetailBeforeReflectionBiasedAdd",
+				"MaskInverseDetailAfterReflectionBiasedMultiply",
+				"MaskInverseDetailAfterReflectionMultiply",
+				"MaskInverseDetailAfterReflectionBiasedAdd",
+
+				"MaskDetailBeforeReflectionBiasedMultiplyComplexFog",
+				"MaskDetailBeforeReflectionMultiplyComplexFog",
+				"MaskDetailBeforeReflectionBiasedAddComplexFog",
+				"MaskDetailAfterReflectionBiasedMultiplyComplexFog",
+				"MaskDetailAfterReflectionMultiplyComplexFog",
+				"MaskDetailAfterReflectionBiasedAddComplexFog",
+				"MaskInverseDetailBeforeReflectionBiasedMultiplyComplexFog",
+				"MaskInverseDetailBeforeReflectionMultiplyComplexFog",
+				"MaskInverseDetailBeforeReflectionBiasedAddComplexFog",
+				"MaskInverseDetailAfterReflectionBiasedMultiplyComplexFog",
+				"MaskInverseDetailAfterReflectionMultiplyComplexFog",
+				"MaskInverseDetailAfterReflectionBiasedAddComplexFog",
+				
+				"MaskDetailBeforeReflectionBiasedMultiplyNormalMap",
+				"MaskDetailBeforeReflectionMultiplyNormalMap",
+				"MaskDetailBeforeReflectionBiasedAddNormalMap",
+				"MaskDetailAfterReflectionBiasedMultiplyNormalMap",
+				"MaskDetailAfterReflectionMultiplyNormalMap",
+				"MaskDetailAfterReflectionBiasedAddNormalMap",
+				"MaskInverseDetailBeforeReflectionBiasedMultiplyNormalMap",
+				"MaskInverseDetailBeforeReflectionMultiplyNormalMap",
+				"MaskInverseDetailBeforeReflectionBiasedAddNormalMap",
+				"MaskInverseDetailAfterReflectionBiasedMultiplyNormalMap",
+				"MaskInverseDetailAfterReflectionMultiplyNormalMap",
+				"MaskInverseDetailAfterReflectionBiasedAddNormalMap",
+
+				"MaskDetailBeforeReflectionBiasedMultiplyComplexFogNormalMap",
+				"MaskDetailBeforeReflectionMultiplyComplexFogNormalMap",
+				"MaskDetailBeforeReflectionBiasedAddComplexFogNormalMap",
+				"MaskDetailAfterReflectionBiasedMultiplyComplexFogNormalMap",
+				"MaskDetailAfterReflectionMultiplyComplexFogNormalMap",
+				"MaskDetailAfterReflectionBiasedAddComplexFogNormalMap",
+				"MaskInverseDetailBeforeReflectionBiasedMultiplyComplexFogNormalMap",
+				"MaskInverseDetailBeforeReflectionMultiplyComplexFogNormalMap",
+				"MaskInverseDetailBeforeReflectionBiasedAddComplexFogNormalMap",
+				"MaskInverseDetailAfterReflectionBiasedMultiplyComplexFogNormalMap",
+				"MaskInverseDetailAfterReflectionMultiplyComplexFogNormalMap",
+				"MaskInverseDetailAfterReflectionBiasedAddComplexFogNormalMap",
+			};
+
+			static IDirect3DPixelShader9* g_environment_no_pixel_shaders[24];
+			static IDirect3DPixelShader9* g_self_illumination_pixel_shaders[48];
+			static IDirect3DPixelShader9* g_change_color_pixel_shaders[48];
+			static IDirect3DPixelShader9* g_multipurpose_pixel_shaders[48];
+			static IDirect3DPixelShader9* g_reflection_pixel_shaders[48];
+			static IDirect3DPixelShader9* g_no_pixel_shaders[24];
 
 			static bool							g_shader_files_present = false;
+			static BOOL							g_using_normal_maps = false;;
 			static ps_2_x_support				g_ps_support = _ps_2_x_support_none;
 			static s_vertex_shader_variables	g_vertex_shader_variables;
 			static s_pixel_shader_variables		g_pixel_shader_variables;
 
+			// hooks to corrently skip past inverse shaders in the usage id list
+			API_FUNC_NAKED static void Hook_EnvironmentNoPixelShaderIDOffset()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_ENVIRONMENT_NO_USAGE_ID_OFFSET_RETN);
+
+				_asm{
+					mov		eax, edi
+					cmp     esi, 6
+					jl		id_offset_retn
+					lea     eax, [edi+300h]
+					cmp		esi, 12
+					jl		id_offset_retn
+					lea		eax, [edi+600h]
+					cmp		esi, 18
+					jl		id_offset_retn
+					lea		eax, [edi+900h]
+id_offset_retn:
+					jmp		RETN_ADDRESS
+				};
+			}
+			API_FUNC_NAKED static void Hook_NoPixelShaderIDOffset()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_NO_USAGE_ID_OFFSET_RETN);
+
+				_asm{
+					mov		eax, edi
+					cmp     esi, 6
+					jl		id_offset_retn
+					lea     eax, [edi+300h]
+					cmp		esi, 12
+					jl		id_offset_retn
+					lea		eax, [edi+600h]
+					cmp		esi, 18
+					jl		id_offset_retn
+					lea		eax, [edi+900h]
+id_offset_retn:
+					jmp		RETN_ADDRESS
+				};
+			}
+			// hooks to return the normal map pixel shaders when applicable by offseting the shader index
+			API_FUNC_NAKED static void Hook_EnvironmentNoPixelShader()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_PS_INDEX_ENVIRONMENT_NO_RETN);
+
+				_asm {
+					push	ebx
+					mov		ebx, g_using_normal_maps
+					test	bl, bl
+					jz		no_normal_maps
+					add		ecx, 0Ch
+no_normal_maps:
+					pop		ebx
+					mov		edi, g_environment_no_pixel_shaders[ecx * 4]
+					jmp		RETN_ADDRESS
+				};
+			}
+			API_FUNC_NAKED static void Hook_EnvironmentNoInversePixelShader()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_PS_INDEX_ENVIRONMENT_NO_INV_RETN);
+
+				_asm {
+					push	ebx
+					mov		ebx, g_using_normal_maps
+					test	bl, bl
+					jz		no_normal_maps
+					add		eax, 0Ch
+no_normal_maps:
+					pop		ebx
+					mov		edi, g_environment_no_pixel_shaders[18h + eax * 4]
+					jmp		RETN_ADDRESS
+				};
+			}
+			API_FUNC_NAKED static void Hook_ChangeColorPixelShader()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_PS_INDEX_CHANGE_COLOR_RETN);
+
+				_asm {
+					push	ebx
+					mov		ebx, g_using_normal_maps
+					test	bl, bl
+					jz		no_normal_maps
+					add		esi, 18h
+no_normal_maps:
+					pop		ebx
+					mov		eax, g_change_color_pixel_shaders[esi * 4]
+					jmp		RETN_ADDRESS
+				};
+			}	
+			API_FUNC_NAKED static void Hook_ChangeColorInversePixelShader()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_PS_INDEX_CHANGE_COLOR_INV_RETN);
+
+				_asm {
+					push	ebx
+					mov		ebx, g_using_normal_maps
+					test	bl, bl
+					jz		no_normal_maps
+					add		esi, 18h
+no_normal_maps:
+					pop		ebx
+					mov		eax, g_change_color_pixel_shaders[18h + esi * 4]
+					jmp		RETN_ADDRESS
+				};
+			}	
+			API_FUNC_NAKED static void Hook_MultipurposePixelShader()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_PS_INDEX_MULTIPURPOSE_RETN);
+
+				_asm {
+					push	ebx
+					mov		ebx, g_using_normal_maps
+					test	bl, bl
+					jz		no_normal_maps
+					add		esi, 18h
+no_normal_maps:
+					pop		ebx
+					mov		eax, g_multipurpose_pixel_shaders[esi * 4]
+					jmp		RETN_ADDRESS
+				};
+			}	
+			API_FUNC_NAKED static void Hook_MultipurposeInversePixelShader()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_PS_INDEX_MULTIPURPOSE_INV_RETN);
+
+				_asm {
+					push	ebx
+					mov		ebx, g_using_normal_maps
+					test	bl, bl
+					jz		no_normal_maps
+					add		esi, 18h
+no_normal_maps:
+					pop		ebx
+					mov		eax, g_multipurpose_pixel_shaders[18h + esi * 4]
+					jmp		RETN_ADDRESS
+				};
+			}
+			API_FUNC_NAKED static void Hook_NoPixelShader()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_PS_INDEX_NO_RETN);
+
+				_asm {
+					push	ebx
+					mov		ebx, g_using_normal_maps
+					test	bl, bl
+					jz		no_normal_maps
+					add		esi, 0Ch
+no_normal_maps:
+					pop		ebx
+					mov		eax, g_no_pixel_shaders[esi * 4]
+					jmp		RETN_ADDRESS
+				};
+			}	
+			API_FUNC_NAKED static void Hook_ReflectionPixelShader()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_PS_INDEX_REFLECTION_RETN);
+
+				_asm {
+					push	ebx
+					mov		ebx, g_using_normal_maps
+					test	bl, bl
+					jz		no_normal_maps
+					add		esi, 18h
+no_normal_maps:
+					pop		ebx
+					mov		eax, g_reflection_pixel_shaders[esi * 4]
+					jmp		RETN_ADDRESS
+				};
+			}
+			API_FUNC_NAKED static void Hook_ReflectionInversePixelShader()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_PS_INDEX_REFLECTION_INV_RETN);
+
+				_asm {
+					push	ebx
+					mov		ebx, g_using_normal_maps
+					test	bl, bl
+					jz		no_normal_maps
+					add		esi, 18h
+no_normal_maps:
+					pop		ebx
+					mov		eax, g_reflection_pixel_shaders[18h + esi * 4]
+					jmp		RETN_ADDRESS
+				};
+			}
+			API_FUNC_NAKED static void Hook_SelfIlluminationPixelShader()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_PS_INDEX_SELF_ILLUMINATION_RETN);
+
+				_asm {
+					push	ebx
+					mov		ebx, g_using_normal_maps
+					test	bl, bl
+					jz		no_normal_maps
+					add		esi, 18h
+no_normal_maps:
+					pop		ebx
+					mov		eax, g_self_illumination_pixel_shaders[esi * 4]
+					jmp		RETN_ADDRESS
+				};
+			}	
+			API_FUNC_NAKED static void Hook_SelfIlluminationInversePixelShader()
+			{
+				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_PS_INDEX_SELF_ILLUMINATION_INV_RETN);
+
+				_asm {
+					push	ebx
+					mov		ebx, g_using_normal_maps
+					test	bl, bl
+					jz		no_normal_maps
+					add		esi, 18h
+no_normal_maps:
+					pop		ebx
+					mov		eax, g_self_illumination_pixel_shaders[18h + esi * 4]
+					jmp		RETN_ADDRESS
+				};
+			}	
 			API_FUNC_NAKED static void Hook_RenderObject_ForceInvertBackfaceNormals()
 			{
 				static uint32 RETN_ADDRESS = GET_FUNC_PTR(RASTERIZER_MODEL_DRAW_INVERT_BACKFACE_NORMALS_CHECK_RETN);
+
 				_asm{
 					mov     al, 1
 					test    al, al
@@ -105,6 +384,7 @@ namespace Yelo
 				};
 			}
 
+			
 			void		SetTexture(IDirect3DDevice9* pDevice, uint16 sampler, datum_index bitmap_tag_index)
 			{
 				// get the bitmap datum pointer
@@ -139,6 +419,7 @@ namespace Yelo
 
 				if(shader_base->shader.shader_type == Enums::_shader_type_model)
 				{
+					g_using_normal_maps = false;
 					TagGroups::s_shader_model_definition* shader_model = CAST_PTR(TagGroups::s_shader_model_definition*, shader_base);
 					if(shader_model->model.maps.shader_extension.Count == 1)
 					{
@@ -147,6 +428,7 @@ namespace Yelo
 						// setup base normal map
 						if(!extension.base_normal.map.tag_index.IsNull())
 						{
+							g_using_normal_maps = true;
 							DX9::c_gbuffer_system::OutputObjectTBN() = true;
 
 							SetTexture(Yelo::DX9::Direct3DDevice(), 4, extension.base_normal.map.tag_index); 
@@ -162,6 +444,7 @@ namespace Yelo
 						// setup detail normal map 1
 						if(!extension.detail_normals[0].map.tag_index.IsNull())
 						{
+							g_using_normal_maps = true;
 							SetTexture(Yelo::DX9::Direct3DDevice(), 5, extension.detail_normals[0].map.tag_index); 
 							g_pixel_shader_variables.detail_normal_1_interp = 1.0f;
 							g_pixel_shader_variables.detail_normal_1_multiplier = extension.detail_normals[0].modifiers.multiplier;
@@ -175,6 +458,7 @@ namespace Yelo
 						// setup detail normal map 2
 						if(!extension.detail_normals[1].map.tag_index.IsNull())
 						{
+							g_using_normal_maps = true;
 							SetTexture(Yelo::DX9::Direct3DDevice(), 6, extension.detail_normals[1].map.tag_index); 
 							g_pixel_shader_variables.detail_normal_2_interp = 1.0f;
 							g_pixel_shader_variables.detail_normal_2_multiplier = extension.detail_normals[1].modifiers.multiplier;
@@ -202,9 +486,11 @@ namespace Yelo
 					TagGroups::s_shader_environment_definition* shader_environment = 
 						CAST_PTR(TagGroups::s_shader_environment_definition*, shader_base);
 
+					g_using_normal_maps = false;
 					// setup base normal map
 					if(!shader_environment->environment.bump_map.map.tag_index.IsNull())
 					{
+						g_using_normal_maps = true;
 						DX9::c_gbuffer_system::OutputObjectTBN() = true;
 
 						SetTexture(Yelo::DX9::Direct3DDevice(), 4, shader_environment->environment.bump_map.map.tag_index); 
@@ -282,6 +568,48 @@ namespace Yelo
 				Memory::WriteRelativeCall(&BuildEffectCollectionPath, GET_FUNC_VPTR(RASTERIZER_DX9_SHADERS_EFFECT_SHADERS_INITIALIZE__SPRINTF_CALL), true);
 				// Forces the backface normals of a two sided face to be inverted to fix lighting issues on trees
 				Memory::WriteRelativeJmp(&Hook_RenderObject_ForceInvertBackfaceNormals, GET_FUNC_VPTR(RASTERIZER_MODEL_DRAW_INVERT_BACKFACE_NORMALS_CHECK_HOOK), true);
+
+				// change the shader usage id offsets on effects that only have 12 shaders
+				Memory::WriteRelativeJmp(&Hook_EnvironmentNoPixelShaderIDOffset,	GET_FUNC_VPTR(RASTERIZER_MODEL_ENVIRONMENT_NO_USAGE_ID_OFFSET_HOOK), true);
+				Memory::WriteRelativeJmp(&Hook_NoPixelShaderIDOffset,				GET_FUNC_VPTR(RASTERIZER_MODEL_NO_USAGE_ID_OFFSET_HOOK), true);
+				
+				int32 i;
+				// change shader usage id pointers to the extended array
+				for(i = 0; i < NUMBEROF(K_SHADER_USAGE_ID_ARRAY_REFERENCES); i++)
+					*K_SHADER_USAGE_ID_ARRAY_REFERENCES[i] = CAST_PTR(void*, &g_shader_usage_id_list[0][0]);
+
+				// update with the extended shader count
+				GET_PTR(RASTERIZER_MODEL_SHADER_LOAD_COUNT) = 48;
+				GET_PTR(RASTERIZER_MODEL_SHADER_LOAD_NO_INVERSE_COUNT) = 24;
+
+
+				// redirect references to the original pixel shader arrays to the custom arrays
+				for(i = 0; i < NUMBEROF(K_PIXEL_SHADER_REFERENCES_ENVIRONMENT_NO); i++)
+					*K_PIXEL_SHADER_REFERENCES_ENVIRONMENT_NO[i] = &g_environment_no_pixel_shaders;
+				for(i = 0; i < NUMBEROF(K_PIXEL_SHADER_REFERENCES_CHANGE_COLOR); i++)
+					*K_PIXEL_SHADER_REFERENCES_CHANGE_COLOR[i] = &g_change_color_pixel_shaders;
+				for(i = 0; i < NUMBEROF(K_PIXEL_SHADER_REFERENCES_MULTIPURPOSE); i++)
+					*K_PIXEL_SHADER_REFERENCES_MULTIPURPOSE[i] = &g_multipurpose_pixel_shaders;
+				for(i = 0; i < NUMBEROF(K_PIXEL_SHADER_REFERENCES_NO); i++)
+					*K_PIXEL_SHADER_REFERENCES_NO[i] = &g_no_pixel_shaders;
+				for(i = 0; i < NUMBEROF(K_PIXEL_SHADER_REFERENCES_REFLECTION); i++)
+					*K_PIXEL_SHADER_REFERENCES_REFLECTION[i] = &g_reflection_pixel_shaders;
+				for(i = 0; i < NUMBEROF(K_PIXEL_SHADER_REFERENCES_SELF_ILLUMINATION); i++)
+					*K_PIXEL_SHADER_REFERENCES_SELF_ILLUMINATION[i] = &g_self_illumination_pixel_shaders;
+
+
+				// change the pixel shader index to return the normal map shaders when appropriate
+				Memory::WriteRelativeJmp(&Hook_EnvironmentNoPixelShader, GET_FUNC_VPTR(RASTERIZER_MODEL_PS_INDEX_ENVIRONMENT_NO_HOOK), true);
+				Memory::WriteRelativeJmp(&Hook_EnvironmentNoInversePixelShader, GET_FUNC_VPTR(RASTERIZER_MODEL_PS_INDEX_ENVIRONMENT_NO_INV_HOOK), true);
+				Memory::WriteRelativeJmp(&Hook_ChangeColorPixelShader, GET_FUNC_VPTR(RASTERIZER_MODEL_PS_INDEX_CHANGE_COLOR_HOOK), true);
+				Memory::WriteRelativeJmp(&Hook_ChangeColorInversePixelShader, GET_FUNC_VPTR(RASTERIZER_MODEL_PS_INDEX_CHANGE_COLOR_INV_HOOK), true);
+				Memory::WriteRelativeJmp(&Hook_MultipurposePixelShader, GET_FUNC_VPTR(RASTERIZER_MODEL_PS_INDEX_MULTIPURPOSE_HOOK), true);
+				Memory::WriteRelativeJmp(&Hook_MultipurposeInversePixelShader, GET_FUNC_VPTR(RASTERIZER_MODEL_PS_INDEX_MULTIPURPOSE_INV_HOOK), true);
+				Memory::WriteRelativeJmp(&Hook_NoPixelShader, GET_FUNC_VPTR(RASTERIZER_MODEL_PS_INDEX_NO_HOOK), true);
+				Memory::WriteRelativeJmp(&Hook_ReflectionPixelShader, GET_FUNC_VPTR(RASTERIZER_MODEL_PS_INDEX_REFLECTION_HOOK), true);
+				Memory::WriteRelativeJmp(&Hook_ReflectionInversePixelShader, GET_FUNC_VPTR(RASTERIZER_MODEL_PS_INDEX_REFLECTION_INV_HOOK), true);
+				Memory::WriteRelativeJmp(&Hook_SelfIlluminationPixelShader, GET_FUNC_VPTR(RASTERIZER_MODEL_PS_INDEX_SELF_ILLUMINATION_HOOK), true);
+				Memory::WriteRelativeJmp(&Hook_SelfIlluminationInversePixelShader, GET_FUNC_VPTR(RASTERIZER_MODEL_PS_INDEX_SELF_ILLUMINATION_INV_HOOK), true);
 			}
 
 			HRESULT		SetVertexShaderConstantF(IDirect3DDevice9* pDevice, UINT StartRegister, CONST float* pConstantData, UINT Vector4fCount)
