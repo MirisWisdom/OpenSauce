27a61658cfe80c59ab3821a0ca8f2cfbec6d3c92
Mark <markrizzo22@gmail.com>
2011-05-05 18:57:57 +0100

    Integrated Yelo Neighborhood

---
+++ Xbox/Xbox1/Yelo Neighborhood/Updater/Downloader.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Updater
+{
+    partial class Downloader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.probar = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtChangeLog = new System.Windows.Forms.TextBox();
+            this.cmdClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // probar
+            // 
+            this.probar.Location = new System.Drawing.Point(12, 232);
+            this.probar.Name = "probar";
+            this.probar.Size = new System.Drawing.Size(338, 23);
+            this.probar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.probar.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 216);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(78, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Downloading...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Change Log";
+            // 
+            // txtChangeLog
+            // 
+            this.txtChangeLog.Enabled = false;
+            this.txtChangeLog.Location = new System.Drawing.Point(12, 25);
+            this.txtChangeLog.Multiline = true;
+            this.txtChangeLog.Name = "txtChangeLog";
+            this.txtChangeLog.Size = new System.Drawing.Size(338, 188);
+            this.txtChangeLog.TabIndex = 3;
+            // 
+            // cmdClose
+            // 
+            this.cmdClose.Location = new System.Drawing.Point(100, 232);
+            this.cmdClose.Name = "cmdClose";
+            this.cmdClose.Size = new System.Drawing.Size(164, 23);
+            this.cmdClose.TabIndex = 4;
+            this.cmdClose.Text = "Close";
+            this.cmdClose.UseVisualStyleBackColor = true;
+            this.cmdClose.Visible = false;
+            this.cmdClose.Click += new System.EventHandler(this.cmdClose_Click);
+            // 
+            // Downloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 267);
+            this.Controls.Add(this.cmdClose);
+            this.Controls.Add(this.txtChangeLog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.probar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Downloader";
+            this.Text = "Updater";
+            this.Load += new System.EventHandler(this.Downloader_Load);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Downloader_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar probar;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtChangeLog;
+        private System.Windows.Forms.Button cmdClose;
+    }
+}
+
---
+++ Xbox/Xbox1/Yelo Neighborhood/Yelo Neighborhood/Screenshot Tools/ScreenshotTool.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using YeloDebug;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Yelo_Neighborhood
+{
+    public partial class ScreenshotTool : Form
+    {
+        List<Image> Images = new List<Image>();
+        ImageFormatSelector IFS = new ImageFormatSelector();
+
+        public ScreenshotTool()
+        {
+            InitializeComponent();
+            cboViewStyle.Items.Add(ImageLayout.Center);
+            cboViewStyle.Items.Add(ImageLayout.Stretch);
+            cboViewStyle.SelectedIndex = 0;
+        }
+
+        public void TakeScreenshot()
+        {
+            listImages.Items.Add(new ListViewItem(DateTime.Now.ToString(), imageList.Images.Count, listImages.Groups[0]));
+            Pauser.Reset();
+            if(LiveStreamRunning) Waiter.WaitOne();
+            Image screenshot = Program.XBox.Screenshot();
+            Pauser.Set();
+            Images.Add(screenshot);
+            imageList.Images.Add(screenshot);
+            if (!checkLiveStream.Checked)
+            {
+                listImages.SelectedItems.Clear();
+                listImages.SelectedIndices.Clear();
+                listImages.Items[listImages.Items.Count - 1].Selected = true;
+            }
+            cmdSaveChecked.Enabled = true;
+            cmdSaveSelected.Enabled = true;
+        }
+
+        private void ScreenshotTool_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            { 
+                case Keys.F5:
+                    TakeScreenshot();
+                    break;
+            }
+        }
+
+        private void captureF5ToolStripMenuItem_Click(object sender, EventArgs e)
+        { TakeScreenshot(); }
+
+        private void listImages_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        { imageBox.BackgroundImage = Images[e.Item.ImageIndex]; }
+
+        private void checkLiveStream_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkLiveStream.Checked)
+            {
+                checkLiveStream.Text = "Live Stream (On)";
+                new Thread(new ParameterizedThreadStart(LiveStream)).Start(imageBox);
+            }
+            else
+            {
+                LiveStreamRunning = false;
+                checkLiveStream.Text = "Live Stream (Off)";
+            }
+        }
+
+        static bool LiveStreamRunning = false;
+        static ManualResetEvent Pauser = new ManualResetEvent(true);
+        static ManualResetEvent Waiter = new ManualResetEvent(false);
+        private static void LiveStream(object pictureBox)
+        {
+            PictureBox imageBox = (PictureBox)pictureBox;
+            Program.XBox.SetFileCacheSize(1);
+            XboxVideoStream xvs = new XboxVideoStream(Program.XBox, XboxVideoStream.VideoSize.Medium, XboxVideoStream.VideoQuality.Regular, XboxVideoStream.VideoPresentationInterval.Immediate);
+            LiveStreamRunning = true;
+            xvs.Begin();
+            while (LiveStreamRunning)
+            {
+                Waiter.Reset();
+                imageBox.BackgroundImage = xvs.NextFrame();//imageBox.BackgroundImage = Program.XBox.Screenshot();
+                Waiter.Set();
+                Pauser.WaitOne();
+            }
+            xvs.End();
+            Program.ScreenshotTool.checkLiveStream.Checked = false;
+        }
+
+        private void ScreenshotTool_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+            LiveStreamRunning = false;
+        }
+
+        private void cmdSaveChecked_Click(object sender, EventArgs e)
+        {
+            if (FBD.ShowDialog() != DialogResult.OK) return;
+            if (IFS.ShowDialog() != DialogResult.OK) return;
+
+            foreach (ListViewItem lvt in listImages.CheckedItems)
+            {
+                string name = lvt.Text;
+                foreach (char c in Path.GetInvalidFileNameChars()) name = name.Replace(c, '_');
+                MemoryStream ms = new MemoryStream();
+                Images[lvt.ImageIndex].Save(ms, IFS.ImageFormat);
+                byte[] data = new byte[(int)ms.Length];
+                ms.Read(data, 0, data.Length);
+                ms.Close();
+                FileStream fs = new FileStream(Path.Combine(FBD.SelectedPath, name) + "." + IFS.ImageFormat.ToString().ToLower(), FileMode.Create);
+                fs.Write(data, 0, data.Length);
+                fs.Flush();
+                fs.Close();
+            }
+        }
+
+        private void cmdSaveSelected_Click(object sender, EventArgs e)
+        {
+            if (FBD.ShowDialog() != DialogResult.OK) return;
+            if (IFS.ShowDialog() != DialogResult.OK) return;
+
+            foreach (ListViewItem lvt in listImages.SelectedItems)
+            {
+                string name = lvt.Text;
+                foreach (char c in Path.GetInvalidFileNameChars()) name = name.Replace(c, '_');
+                MemoryStream ms = new MemoryStream();
+                Images[lvt.ImageIndex].Save(ms, IFS.ImageFormat);
+                byte[] data = new byte[(int)ms.Length];
+                ms.Read(data, 0, data.Length);
+                ms.Close();
+                FileStream fs = new FileStream(Path.Combine(FBD.SelectedPath, name) + "." + IFS.ImageFormat.ToString().ToLower(), FileMode.Create);
+                fs.Write(data, 0, data.Length);
+                fs.Flush();
+                fs.Close();
+            }
+        }
+
+        private void cboViewStyle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            imageBox.BackgroundImageLayout = (ImageLayout)cboViewStyle.SelectedItem;
+        }
+    }
+}
