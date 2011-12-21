c8539705ea1f0a09b3ba5b5deb3b3e605a54b374
kornman00 <kornman00@gmail.com>
2011-12-21 09:58:44 -0600

    * Halo1_CE: Updated the license headers with the new format * OpenSauce_XLive: Updated the license headers with the new format

--- OpenSauce/Halo1/Halo1_CE/Game/Campaign.cpp
+++ OpenSauce/Halo1/Halo1_CE/Game/Campaign.cpp
@@ -1,20 +1,8 @@
 /*
-    Yelo: Open Sauce SDK
+	Yelo: Open Sauce SDK
 		Halo 1 (CE) Edition
-    Copyright (C) 2005-2010  Kornner Studios (http://kornner.com)
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	See license\OpenSauce\Halo1_CE for specific license information
 */
 #include "Common/Precompile.hpp"
 #include "Game/Campaign.hpp"
