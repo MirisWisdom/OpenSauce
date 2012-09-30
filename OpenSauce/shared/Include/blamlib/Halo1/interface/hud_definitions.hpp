a18d08bdda00bd18a3fcdf19bba132f08d8cafab
kornman00 <kornman00@gmail.com>
2012-09-30 05:00:42 -0700

    * Halo1_CE: Renamed UIWidget to UIWidgets * Halo1_CE: Removed local_player_index from display_scripted_ui_widget (requires rebuild of CheApe). Implementation just uses NONE now (this should fix the crash problem, but I wasn't given a test map to verify this yet)
    !!! MEGA NOTE: While RE'ing the ui crap, I found myself in the campaign level code again and realized that there are some stack buffers fixed to 10 elements, plus the player profile has a fixed buffer as well. Going to look into implementing tag-driven overriding of the campaign crap instead, sometime in the future
    !!! TL;DR: Don't touch Campaign.hpp/.cpp/.inl unless your name is k0rnm4nOO.

---
+++ OpenSauce/shared/Include/blamlib/Halo1/interface/hud_definitions.hpp
@@ -0,0 +1,88 @@
+/*
+	Yelo: Open Sauce SDK
+		Halo 1 (CE) Edition
+
+	See license\OpenSauce\Halo1_CE for specific license information
+*/
+#pragma once
+
+namespace Yelo
+{
+	namespace Enums
+	{
+		enum hud_anchor : _enum
+		{
+			_hud_anchor_top_left,
+			_hud_anchor_top_right,
+			_hud_anchor_bottom_left,
+			_hud_anchor_bottom_right,
+			_hud_anchor_center,
+
+			_hud_anchor,
+		};
+	};
+
+	namespace Flags
+	{
+		enum hud_scaling_flags : word_flags
+		{
+			_hud_scaling_dont_scale_offset_bit,
+			_hud_scaling_dont_scale_size_bit,
+			_hud_scaling_use_high_res_scale_bit,
+		};
+	};
+
+	namespace TagGroups
+	{
+		struct s_hud_absolute_placement
+		{
+			TAG_ENUM(anchor, Enums::hud_anchor);
+			PAD16;
+			TAG_PAD(int32, 8);
+		}; BOOST_STATIC_ASSERT( sizeof(s_hud_absolute_placement) == 0x24 );
+
+		struct s_hud_element // same size as s_hud_absolute_placement...I think this may be union'd
+		{
+			TAG_FIELD(point2d, anchor_offset);
+			TAG_FIELD(real, width_scale);
+			TAG_FIELD(real, height_scale);
+			TAG_FIELD(Flags::hud_scaling_flags, scaling_flags);
+			PAD16;
+			TAG_PAD(int32, 5);
+		}; BOOST_STATIC_ASSERT( sizeof(s_hud_element) == 0x24 );
+
+		struct icon_hud_element_definition
+		{
+			TAG_FIELD(int16, sequence_index);
+			TAG_FIELD(int16, width_offset);
+			TAG_FIELD(point2d, offset_from_reference_corner);
+			TAG_FIELD(argb_color, override_icon_color);
+			TAG_FIELD(sbyte, frame_rate);
+			TAG_FIELD(byte_flags, flags);
+			TAG_FIELD(int16, text_index);
+		}; BOOST_STATIC_ASSERT( sizeof(icon_hud_element_definition) == 0x10 );
+
+
+		struct hud_messaging_definition
+		{
+			s_hud_absolute_placement placement;
+			s_hud_element element;
+			TAG_FIELD(tag_reference, single_player_font, 'font');
+			TAG_FIELD(tag_reference, multiplayer_font, 'font');
+			TAG_FIELD(real, up_time);
+			TAG_FIELD(real, fade_time);
+			TAG_FIELD(real_argb_color, icon_color);
+			TAG_FIELD(real_argb_color, text_color);
+			TAG_FIELD(real, text_spacing);
+			TAG_FIELD(tag_reference, item_message_text, 'ustr');
+			TAG_FIELD(tag_reference, icon_bitmap, 'bitm');
+			TAG_FIELD(tag_reference, alternate_icon_text, 'ustr');
+			TAG_TBLOCK(button_icons, icon_hud_element_definition);
+		}; BOOST_STATIC_ASSERT( sizeof(hud_messaging_definition) == 0xD0 );
+
+		struct hud_globals_definition
+		{
+			hud_messaging_definition hud_messaging;
+		}; //BOOST_STATIC_ASSERT( sizeof(hud_globals_definition) == 0x );
+	};
+};
