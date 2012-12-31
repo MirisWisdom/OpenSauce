c39ecfd04fd062dd8d11489c0465d08835de0640
TheFieryScythe <thefieryscythe@gmail.com>
2012-12-31 18:58:04 +0000

    * Halo1_CE: Shader and code fix for issue 53. * Halo1_CE: Fix for Halo not loading when using the -use* command line argument. * Halo1_CE: Updated Readme. * Halo1_Installer: Added web launcher custom action (Native Wix hyperlink support is only in Win 7+). * Halo1_Installer: Added link to Google code page on welcome dialog.

---
+++ OpenSauce/Halo1/Halo1_Installer/WebLauncher/WebLauncher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace WebLauncher
+{
+	public class CustomActions
+	{
+		[CustomAction]
+		public static ActionResult LaunchWebsite(Session session)
+		{
+			session.Log("Begin WebLauncher");
+
+			string URL = session["WEBLAUNCHER_URL"];
+
+			System.Diagnostics.Process.Start(URL);
+
+			return ActionResult.Success;
+		}
+	}
+}
