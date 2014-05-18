ccbd4ea674b5339681ea5b398117abb9a65fe9d7
kornman00 <kornman00@gmail.com>
2014-05-18 11:32:50 -0500

    * Halo1_CE: Changed Hook_D3DResetCall to be a RelativeCall to help HAC2 to be more compatible with OS * Halo1_CE: Refactored DeviceHooks (we forward declared our static functions in a public hpp)

--- OpenSauce/Halo1/Halo1_CE/Rasterizer/DX9/DeviceHooks.hpp
+++ OpenSauce/Halo1/Halo1_CE/Rasterizer/DX9/DeviceHooks.hpp
@@ -13,19 +13,6 @@ namespace Yelo
 	{
 		void Initialize();
 		void Dispose();
-
-		void CreateD3DHook(void* call_address, void* hook_address);
-
-		HRESULT CreateDevice(IDirect3D9* d3d, UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD BehaviorFlags, D3DPRESENT_PARAMETERS *pPresentationParameters, IDirect3DDevice9 **ppReturnedDeviceInterface);
-
-		HRESULT Reset(IDirect3DDevice9* device, D3DPRESENT_PARAMETERS *pPresentationParameters);
-
-		HRESULT BeginScene(IDirect3DDevice9* device);
-		HRESULT EndScene(IDirect3DDevice9* device);
-
-		HRESULT SetVertexShaderConstantF_ViewProj(IDirect3DDevice9* device, UINT StartRegister, CONST float* pConstantData, UINT Vector4fCount);
-		HRESULT SetVertexShaderConstantF_ModelSpecColor(IDirect3DDevice9* device, UINT StartRegister, CONST float* pConstantData, UINT Vector4fCount);
-		HRESULT SetVertexShaderConstantF_ModelVertexLight(IDirect3DDevice9* device, UINT StartRegister, CONST float* pConstantData, UINT Vector4fCount);
 	};};
 };
 #endif
