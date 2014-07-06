bc54c29733dc18cd7820d32b1b8739e914fafa18
Martin Ballantyne <TheFieryScythe@gmail.com>
2014-07-06 21:56:41 +0100

    * Halo1_Installer: Removed CE/HEK registry entry locks. * Halo1_Installer: Removed option to not create HEK exe's. * Halo1_Installer: Remove settings XML editing, it's not required. * Halo1_Installer: Fixed installation failing when not installing OSIDE.

---
+++ OpenSauce/Halo1/Halo1_Installer/SetPathProperties/PathSet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace SetPathProperties
+{
+    [XmlRoot(ElementName = "PathSet")]
+    public class PathSet
+    {
+        public class Property
+        {
+            [XmlAttribute("Name")]
+            public string Name;
+
+            [XmlAttribute("Value")]
+            public string Value;
+        }
+
+        [XmlAttribute("Id")]
+        public string ID;
+
+        [XmlAttribute("RootProperty")]
+        public string RootProperty;
+
+        [XmlElement("Property")]
+        public List<Property> Properties = new List<Property>();
+    }
+}
