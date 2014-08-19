e6c62ab0bc2f8a6c5c7b2573bf37c5d586af5972
Martin Ballantyne <TheFieryScythe@gmail.com>
2014-08-20 00:13:22 +0100

    * Halo1_Installer: Refactored file patcher custom action to be testable. * Halo1_Installer: Re-written the file patcher unit tests.

---
+++ OpenSauce/Halo1/Halo1_Installer/FilePatcher/Patcher/PatchDefinition.cs
@@ -0,0 +1,91 @@
+﻿/*
+	Yelo: Open Sauce SDK
+		Installer
+
+	See license\OpenSauce\OpenSauce for specific license information
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace FilePatcher.Patcher
+{
+	[XmlRoot(ElementName = "FilePatch")]
+	public class PatchDefinition
+	{
+		public enum ValueType
+		{
+			[XmlEnum(Name = "string")]
+			String,
+			[XmlEnum(Name = "uint32")]
+			UInteger32,
+			[XmlEnum(Name = "uint16")]
+			UInteger16,
+			[XmlEnum(Name = "byte")]
+			Byte,
+			[XmlEnum(Name = "bytes")]
+			Bytes
+		}
+
+		public class Address
+		{
+			[XmlAttribute(AttributeName = "ptr")]
+			public string PTRString = "";
+
+			[XmlAttribute(AttributeName = "value")]
+			public string Value = "";
+		}
+
+		public class Data
+		{
+			[XmlAttribute(AttributeName = "id")]
+			public string ID = "";
+
+			[XmlAttribute(AttributeName = "type")]
+			public ValueType Type;
+
+			[XmlAttribute(AttributeName = "length"), DefaultValue(0)]
+			public int Length = 0;
+
+			[XmlAttribute(AttributeName = "swapendian"), DefaultValue(false)]
+			public bool SwapEndian = false;
+
+			[XmlAttribute(AttributeName = "writenull"), DefaultValue(true)]
+			public bool WriteNull = true;
+
+			[XmlText]
+			public string Value;
+		}
+
+		public class Patch
+		{
+			[XmlAttribute(AttributeName = "id")]
+			public string ID = "";
+
+			[XmlElement(ElementName = "Data")]
+			public List<Data> Data = new List<Data>();
+
+			[XmlElement(ElementName = "Address")]
+			public List<Address> Addresses = new List<Address>();
+		}
+
+		[XmlAttribute(AttributeName = "inputfile")]
+		public string InputFile = "";
+
+		[XmlAttribute(AttributeName = "outputfile")]
+		public string OutputFile = "";
+
+		[XmlAttribute(AttributeName = "peaddressmask")]
+		public string PEAddressMaskString = "";
+
+		[XmlAttribute(AttributeName = "md5_pre")]
+		public string MD5Pre = "";
+
+		[XmlAttribute(AttributeName = "md5_post")]
+		public string MD5Post = "";
+
+		[XmlElement(ElementName = "Patch")]
+		public List<Patch> Patches = new List<Patch>();
+	}
+}
---
+++ OpenSauce/Halo1/Halo1_Installer/FilePatcher/Utilities/Parse.cs
@@ -0,0 +1,47 @@
+﻿/*
+	Yelo: Open Sauce SDK
+		Installer
+
+	See license\OpenSauce\OpenSauce for specific license information
+*/
+using System;
+using System.ComponentModel;
+
+namespace FilePatcher.Utilities
+{
+	public static class Parse
+	{
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>	Attempts to parse hexadecimal from the given data. </summary>
+		///
+		/// <tparam name="T">	Generic type parameter. </tparam>
+		/// <param name="value"> 	The hex value. </param>
+		/// <param name="result">	[out] The result. </param>
+		///
+		/// <returns>	true if it succeeds, false if it fails. </returns>
+		public static bool TryParseHex<T>(string value, out T result)
+		{
+			var converter = TypeDescriptor.GetConverter(typeof(T));
+
+			try
+			{
+				// Force interpret the value as hex
+				var valueString = value;
+				if (!valueString.StartsWith("0x"))
+				{
+					valueString = valueString.Insert(0, "0x");
+				}
+
+				result = (T)converter.ConvertFromString(valueString);
+			}
+			catch (Exception)
+			{
+				result = default(T);
+
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
