5361f732d0f4e90640bc40e9ee27e420fa308abf
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2014-08-23 20:57:07 +0100

    * Halo1_Installer: Renamed and refactored SetPathProperties. * Halo1_Installer: Added unit tests for SetProperties project. * Halo1_Installer: OS installation is now side by side with 1.10 using 1.09 exe's.

--- OpenSauce/Halo1/Halo1_Installer/SetPathProperties/Properties/AssemblyInfo.cs
+++ OpenSauce/Halo1/Halo1_Installer/SetPathProperties/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SetPathProperties")]
+[assembly: AssemblyTitle("SetProperties")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SetPathProperties")]
+[assembly: AssemblyProduct("SetProperties")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
