7eae3a389912b78238bdf325f8ec38b56bd74dbe
Sean <1270047+kornman00@users.noreply.github.com>
2014-11-16 20:41:08 -0600

    * Halo1_CheApe: Added fieldset replacement for s_sound_permutation to correct a pad field which has data (decompressed_buffer_size) that should instead be skip'd

--- OpenSauce/Halo1/Halo1_CheApe/TagGroups/TagFieldReplacements/Sound.hpp
+++ OpenSauce/Halo1/Halo1_CheApe/TagGroups/TagFieldReplacements/Sound.hpp
@@ -16,6 +16,26 @@ namespace Yelo
 {
 	namespace TagGroups { namespace TagFieldReplacements { namespace Sound
 	{
+		static void SoundPermutationsBlockFixActualFields(tag_block_definition* sound_permutations_block)
+		{
+			assert(0==strcmp("sound_permutations_block", sound_permutations_block->name));
+
+			// s_sound_permutation->decompressed_buffer_size (actually 'unused0[0]' in engine code) is a postprocessed
+			// field, but GBX left the tag definitions as 'pad' instead of changing it to 'skip'. We correct this here.
+			// +2 as we begin the initial padding, then a new (correct) skip, then the END() logic will finish the existing pad field
+			FIELDSET_REPLACEMENT_BEGIN(s_sound_permutation, 10+2, sound_permutations_block)
+				FIELDSET_SEEK_AFTER_WITH_COPY("next permutation index")
+				FIELDSET_INSERT_BEGIN()
+					FIELDSET_INSERT_PAD(next_permutation_index, 0,
+						sizeof(datum_index) +	// cache_block_index
+						sizeof(void*) +			// cache_base_address
+						sizeof(datum_index)		// cache_tag_index
+						)
+					FIELDSET_INSERT_SKIP(decompressed_buffer_size)
+				FIELDSET_INSERT_END()
+			FIELDSET_REPLACEMENT_END();
+		}
+
 		static void SoundUpdateFlags(tag_block_definition* sound_block)
 		{
 			auto* flags_list = Shared::FindFlagsField<Enums::_field_long_flags>(sound_block);
@@ -41,6 +61,20 @@ namespace Yelo
 			assert(sound);
 
 			SoundUpdateFlags(sound->header_block_definition);
+
+			auto* block = sound->header_block_definition;
+			// find sound->pitch_ranges
+			int field_index = block->FindFieldIndex(Enums::_field_block, "pitch ranges");
+			assert(field_index != NONE);
+
+			block = block->fields[field_index].Definition<tag_block_definition>();
+			// find sound_pitch_range->permutations
+			field_index = block->FindFieldIndex(Enums::_field_block, "permutations");
+			assert(field_index != NONE);
+
+			block = block->fields[field_index].Definition<tag_block_definition>();
+
+			SoundPermutationsBlockFixActualFields(block);
 		}
 	};};}
 };
