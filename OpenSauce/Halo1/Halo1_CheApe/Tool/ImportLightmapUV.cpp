9ee0e658159f3c894c845be6ac8dc7d0bbee9e12
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2014-11-26 16:49:47 +0000

    * Halo1_CheApe: Fixed uv import failing from logic change and incorrect function pointer. * Halo1_CheApe: UV import now uses the number at the end of a group name to match the bsp's lightmap index, rather than relying on the obj group order.

--- OpenSauce/Halo1/Halo1_CheApe/Tool/ImportLightmapUV.cpp
+++ OpenSauce/Halo1/Halo1_CheApe/Tool/ImportLightmapUV.cpp
@@ -24,43 +24,50 @@ namespace Yelo
 		{
 			struct s_vertex_indices
 			{
-				uint32 position_index, normals_index, texcoord_index;
+				const uint32 position_index;
+				const uint32 normals_index;
+				const uint32 texcoord_index;
 
-				s_vertex_indices() : position_index(0), normals_index(0), texcoord_index(0)
-				{
-				}
+				s_vertex_indices()
+					: position_index(0)
+					, normals_index(0)
+					, texcoord_index(0)
+				{ }
 			};
+
 			struct s_face
 			{
 				s_vertex_indices vertex_indicies[3];
 
-				s_face() : vertex_indicies()
-				{
-				}
+				s_face()
+					: vertex_indicies()
+				{ }
 			};
+
 			struct s_group
 			{
 				static const size_t k_name_length = 255;
 
 				std::vector<s_face> faces;
-				char name[k_name_length + 1];
+				const std::string group_name;
+				const int lightmap_index;
 
-				s_group(cstring name) : faces()
-				{
-					size_t name_length = strlen(name);
-					YELO_ASSERT(name_length <= k_name_length);
-
-					strcpy_s(this->name, sizeof(this->name), name);
-				}
+				s_group(cstring name, const int index)
+					: faces()
+					, group_name(name)
+					, lightmap_index(index)
+				{ }
 			};
 
 			std::vector<real_point2d> m_texcoords;
-			std::vector<s_group*> m_groups;
+			std::vector<std::shared_ptr<s_group>> m_groups;
 
 		public:
-			c_obj_file() : m_texcoords(), m_groups()
-			{
-			}
+			c_obj_file()
+				: m_texcoords()
+				, m_groups()
+			{ }
+
 			~c_obj_file();
 
 			HRESULT LoadFromFile(const std::string& obj_path);
@@ -78,41 +85,45 @@ namespace Yelo
 				cstring obj_file;
 			}* args = CAST_PTR(s_arguments*, arguments);
 
+			// Load the BSP tag and check it is not readonly
 			datum_index bsp_index = blam::tag_load<TagGroups::structure_bsp>(args->bsp_name, FLAG(Flags::_tag_load_non_resolving_references_bit));
 
-			if(!bsp_index.IsNull() && !blam::tag_read_only(bsp_index))
+			if(bsp_index.IsNull() || blam::tag_read_only(bsp_index))
 			{
-				HRESULT hr = S_OK;
-
-				TagGroups::structure_bsp* bsp = blam::tag_get<TagGroups::structure_bsp>(bsp_index);
-
-				std::string obj_file_path(Yelo::Settings::c_settings_cheape::Profile().GetDataPath());
-				StringEditing::AppendCharIfAbsent(obj_file_path, '\\');
-
-				cstring obj_argument = args->obj_file;
-				while(obj_argument[0] == '\\')
-					obj_argument++;
-
-				obj_file_path.append(obj_argument);
-
-				bool has_extension = false;
-				// TODO: why minus 4?
-				if(obj_file_path.find(".obj", obj_file_path.length() - 4) != std::string::npos)
-					has_extension = true;
-
-				if(!has_extension)
-					obj_file_path.append(".obj");
-
-				auto obj_file = c_obj_file();
-				if(SUCCEEDED(hr)) hr = obj_file.LoadFromFile(obj_file_path);
-				if(SUCCEEDED(hr)) hr = obj_file.CheckBspCompatibility(bsp);
-				if(SUCCEEDED(hr)) hr = obj_file.ReplaceVertexUVs(bsp);
-				if(SUCCEEDED(hr)) hr = blam::tag_save(bsp_index) ? S_OK : E_FAIL;
-				if(FAILED(hr))
-					YELO_WARN("OS_tool: failed to replace lightmap-uvs for structure_bsp: %s", args->bsp_name);
-			}
-			else
 				YELO_WARN("OS_tool: failed to open structure_bsp for editing: %s", args->bsp_name);
+				return;
+			}
+
+			auto* bsp = blam::tag_get<TagGroups::structure_bsp>(bsp_index);
+
+			// Build the obj file path
+			std::string obj_file_path(Yelo::Settings::c_settings_cheape::Profile().GetDataPath());
+			StringEditing::AppendCharIfAbsent(obj_file_path, '\\');
+
+			// Skip any leading folder dividers
+			cstring obj_argument = args->obj_file;
+			while((obj_argument[0] == '\\') || (obj_argument[0] == '/'))
+			{
+				obj_argument++;
+			}
+
+			obj_file_path.append(obj_argument);
+
+			// Add the obj extension if missing
+			if(obj_file_path.find(".obj") == std::string::npos)
+			{
+				obj_file_path.append(".obj");
+			}
+
+			auto obj_file = c_obj_file();
+
+			HRESULT hr = S_OK;
+			if(SUCCEEDED(hr)) hr = obj_file.LoadFromFile(obj_file_path);
+			if(SUCCEEDED(hr)) hr = obj_file.CheckBspCompatibility(bsp);
+			if(SUCCEEDED(hr)) hr = obj_file.ReplaceVertexUVs(bsp);
+			if(SUCCEEDED(hr)) hr = blam::tag_save(bsp_index) ? S_OK : E_FAIL;
+			if(FAILED(hr))
+				YELO_WARN("OS_tool: failed to replace lightmap-uvs for structure_bsp: %s", args->bsp_name);
 		}
 	};
 };
