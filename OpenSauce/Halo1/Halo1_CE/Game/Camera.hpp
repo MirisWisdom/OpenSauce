dbd2ff598570162321df2c624bdb7c5d47e14068
Martin Ballantyne <TheFieryScythe@gmail.com>
2015-01-09 03:30:57 +0000

    * Halo1_CE: Re-written FOV modifier. * Halo1_CE: Added options to disable FOV modifier in main menu and cinematics. * Halo1_CE: Fixed VS trying to load os_haloce.exe when debugging.

--- OpenSauce/Halo1/Halo1_CE/Game/Camera.hpp
+++ OpenSauce/Halo1/Halo1_CE/Game/Camera.hpp
@@ -8,6 +8,9 @@
 
 #if PLATFORM_IS_USER
 #include <blamlib/Halo1/camera/director.hpp>
+#include <YeloLib/configuration/c_configuration_value.hpp>
+#include <YeloLib/configuration/c_configuration_container.hpp>
+#include <YeloLib/open_sauce/settings/c_settings_singleton.hpp>
 
 namespace Yelo
 {
@@ -184,21 +187,35 @@ namespace Yelo
 
 	namespace Fov
 	{
+		class c_settings_container
+			: public Configuration::c_configuration_container
+		{
+		public:
+			Configuration::c_configuration_value<real> m_field_of_view;
+			Configuration::c_configuration_value<bool> m_ignore_fov_change_in_cinematics;
+			Configuration::c_configuration_value<bool> m_ignore_fov_change_in_main_menu;
+
+			c_settings_container();
+			
+		protected:
+			const std::vector<i_configuration_value* const> GetMembers() final override;
+		};
+
+		class c_settings_fov
+			: public Settings::c_settings_singleton<c_settings_container, c_settings_fov>
+		{
+		public:
+			void PostLoad() final override;
+
+			void PreSave() final override;
+		};
+
 		void Initialize();
 		void Dispose();
-		void Update();
-		
+
 		real GetFieldOfView();
 		void SetFieldOfView(const real fov);
 		void ResetFieldOfView();
-
-#if defined(DX_WRAPPER) // TODO: Need fallback initialization for when we're not using the DX_WRAPPER
-		void Initialize3D(IDirect3DDevice9 *pDevice, D3DPRESENT_PARAMETERS *pPP);
-		void OnLostDevice();
-		void OnResetDevice(D3DPRESENT_PARAMETERS *pPP);
-		void Render();
-		void Release();
-#endif
 	};
 };
 #endif
