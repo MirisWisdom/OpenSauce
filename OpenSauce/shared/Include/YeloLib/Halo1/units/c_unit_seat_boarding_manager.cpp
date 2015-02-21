beb1608f6565538fd5e14c88cf8c6070292d2e56
Martin Ballantyne <TheFieryScythe@gmail.com>
2015-02-21 22:38:10 +0000

    * Halo1_CE: Fixed boarding delay based on whether a region is destroyed.

--- OpenSauce/shared/Include/YeloLib/Halo1/units/c_unit_seat_boarding_manager.cpp
+++ OpenSauce/shared/Include/YeloLib/Halo1/units/c_unit_seat_boarding_manager.cpp
@@ -478,7 +478,7 @@ namespace Yelo
 				if(TEST_FLAG(seat_boarding.delay_until, Flags::_unit_seat_boarding_delay_until_flags_region_destroyed_bit)
 					&& (seat_boarding.region_index != NONE))
 				{
-					start_boarding &= TEST_FLAG(unit_datum.object.regions_destroyed_flags, seat_boarding.region_index);
+					start_boarding &= TEST_FLAG(parent_unit_datum.object.regions_destroyed_flags, seat_boarding.region_index);
 				}
 			}
 
