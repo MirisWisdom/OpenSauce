be1c4dd6a4d5c25f51638304c59bc788abfd14a2
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2015-09-20 21:51:18 +0100

    * Initial automated test runner

---
+++ OpenSauce/shared/Include/YeloLib/automation/c_automation_runner.hpp
@@ -0,0 +1,42 @@
+/*
+	Yelo: Open Sauce SDK
+		Halo 1 (CE) Edition
+
+	See license\OpenSauce\Halo1_CE for specific license information
+*/
+#pragma once
+
+#ifdef API_DEBUG
+
+namespace Yelo
+{
+    namespace Automation
+    {
+        class i_automated_test;
+        class i_test_list_output;
+        class i_test_run_input;
+        class i_test_run_output;
+
+        class c_automation_runner final
+        {
+            static c_automation_runner m_instance;
+        public:
+            static c_automation_runner& Get();
+
+        private:
+            std::map<const std::string, i_automated_test*> m_registered_tests;
+
+            c_automation_runner();
+
+            void ListTests(i_test_list_output& output) const;
+            void RunTests(i_test_run_input& input, i_test_run_output& output);
+
+        public:
+            void RegisterTest(const std::string& name, i_automated_test* test);
+            void CreateTestList(const std::string& test_list_file) const;
+            void Run(const std::string& test_list_file, const std::string& test_results_file);
+            bool RunTest(const std::string& name);
+        };
+    };
+};
+#endif
