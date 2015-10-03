4835bfbdbb69029887e25b7217183d7d980027dd
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2015-10-03 23:23:27 +0100

    * Replaced OutputDebugString instances with LOG macro. * Bit of refactoring in touched files.

--- OpenSauce/shared/Include/YeloLib/configuration/property_tree/c_property_tree_file.cpp
+++ OpenSauce/shared/Include/YeloLib/configuration/property_tree/c_property_tree_file.cpp
@@ -6,58 +6,55 @@
 #include "Common/Precompile.hpp"
 #include <YeloLib/configuration/property_tree/c_property_tree_file.hpp>
 
-#include <YeloLib/configuration/property_tree/c_property_tree_leaf_iterator.hpp>
 #include <YeloLib/configuration/property_tree/property_tree_logging.hpp>
 
 namespace Yelo
 {
-	namespace Configuration { namespace PropertyTree
-	{
-		c_property_tree_file::c_property_tree_file(const std::string& file_path)
-			: c_property_tree_leaf(m_property_tree_file)
-			, m_file_path(file_path)
-		{
-			m_property_tree_file.clear();
-		}
-		
-		bool c_property_tree_file::Load()
-		{
-			try
-			{
-				ReadPropertyTree(m_file_path, m_property_tree_file);
+    namespace Configuration
+    {
+        namespace PropertyTree
+        {
+            c_property_tree_file::c_property_tree_file(const std::string& file_path)
+                : c_property_tree_leaf(m_property_tree_file),
+                  m_file_path(file_path)
+            {
+                m_property_tree_file.clear();
+            }
 
-			#ifdef PRINT_PROPERTY_TREES
-				PrintPropertyTree(m_property_tree_file);
-			#endif
-			}
-			catch(boost::property_tree::ptree_error e)
-			{ //-V565
-#ifdef DEBUG
-				if(IsDebuggerPresent())
-				{
-					OutputDebugString("Exception occurred when reading a property tree:\n");
-					OutputDebugString(e.what());
-					OutputDebugString("\n");
-				}
+            bool c_property_tree_file::Load()
+            {
+                try
+                {
+                    ReadPropertyTree(m_file_path, m_property_tree_file);
+
+#ifdef PRINT_PROPERTY_TREES
+                    PrintPropertyTree(m_property_tree_file);
 #endif
-				return false;
-			}
-			return true;
-		}
-		
-		void c_property_tree_file::Save()
-		{
-			WritePropertyTree(m_file_path, m_property_tree_file);
-		}
-		
-		void c_property_tree_file::Clear()
-		{
-			m_property_tree_file.clear();
-		}
+                }
+                catch (boost::property_tree::ptree_error e)
+                { //-V565
+#ifdef API_DEBUG
+                    LOG(Error, std::string("Exception occurred when reading a property tree: ") + e.what());
+#endif
+                    return false;
+                }
+                return true;
+            }
 
-		std::unique_ptr<i_configuration_leaf> c_property_tree_file::Root()
-		{
-			return std::unique_ptr<i_configuration_leaf>(new c_property_tree_leaf(m_property_tree_file));
-		}
-	};};
+            void c_property_tree_file::Save()
+            {
+                WritePropertyTree(m_file_path, m_property_tree_file);
+            }
+
+            void c_property_tree_file::Clear()
+            {
+                m_property_tree_file.clear();
+            }
+
+            std::unique_ptr<i_configuration_leaf> c_property_tree_file::Root()
+            {
+                return std::make_unique<c_property_tree_leaf>(m_property_tree_file);
+            }
+        };
+    };
 };
