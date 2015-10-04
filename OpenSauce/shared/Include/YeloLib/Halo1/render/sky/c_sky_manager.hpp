0364e2b1242cef25d6f6cc2eaf8422dc12583428
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2015-10-04 01:04:30 +0100

    * Added tests for c_sky_manager. * Defined out block functions that are editor only.

--- OpenSauce/shared/Include/YeloLib/Halo1/render/sky/c_sky_manager.hpp
+++ OpenSauce/shared/Include/YeloLib/Halo1/render/sky/c_sky_manager.hpp
@@ -6,43 +6,50 @@
 */
 #pragma once
 
-#include <blamlib/Halo1/scenario/scenario_definitions.hpp>
-
 namespace Yelo
 {
-	namespace Render { namespace Sky
-	{
-		class c_sky_manager final
-		{
-			class s_sky_entry final
-			{
-				datum_index m_sky_tag_index;
-				bool m_is_override;
-				PAD24;
-				std::shared_ptr<s_sky_entry> m_original_sky_entry;
+    template<typename T>
+    struct TagBlock;
+    struct tag_reference;
 
-			public:
-				s_sky_entry(const datum_index sky_tag_index);
-				s_sky_entry(const datum_index sky_tag_index, std::shared_ptr<s_sky_entry> sky_entry);
+    namespace Render
+    {
+        namespace Sky
+        {
+            class c_sky_manager final
+            {
+                class s_sky_entry;
+                typedef std::shared_ptr<s_sky_entry> sky_entry_ptr_t;
+                typedef std::vector<sky_entry_ptr_t> sky_entry_list_t;
 
-				bool IsOverride() const;
+                class s_sky_entry final
+                {
+                    datum_index m_sky_tag_index;
+                    bool m_is_override;
+                    PAD24;
+                    sky_entry_ptr_t m_original_sky_entry;
 
-				datum_index GetTagIndex() const;
+                public:
+                    explicit s_sky_entry(const datum_index sky_tag_index);
+                    s_sky_entry(const datum_index sky_tag_index, sky_entry_ptr_t sky_entry);
 
-				std::shared_ptr<s_sky_entry> OriginalSky() const;
-			};
-			typedef std::shared_ptr<s_sky_entry> sky_entry_ptr_t;
-			typedef std::vector<sky_entry_ptr_t> sky_entry_list_t;
+                    bool IsOverride() const;
 
-			sky_entry_list_t m_sky_list;
+                    datum_index GetTagIndex() const;
 
-		public:
-			void Reset();
-			void Clear();
+                    sky_entry_ptr_t OriginalSky() const;
+                };
 
-			void SetScenarioSkies(const TagBlock<tag_reference>& skies);
-			void SetSkyIndex(const byte sky_index, const datum_index sky_tag_index);
-			datum_index GetSkyTagIndex(const byte sky_index) const;
-		};
-	};};
+                sky_entry_list_t m_sky_list;
+
+            public:
+                void Reset();
+                void Clear();
+
+                void SetScenarioSkies(const TagBlock<tag_reference>& skies);
+                void SetSkyIndex(const byte sky_index, const datum_index sky_tag_index);
+                datum_index GetSkyTagIndex(const byte sky_index) const;
+            };
+        };
+    };
 };
