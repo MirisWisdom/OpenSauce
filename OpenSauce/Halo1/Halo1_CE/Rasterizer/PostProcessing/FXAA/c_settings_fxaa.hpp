d38fd68050c267e04701eb9c9a07deb3355b5cc1
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2015-10-11 18:14:51 +0100

    * Halo1_CE: Reformatted FXAA classes

--- OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/FXAA/c_settings_fxaa.hpp
+++ OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/FXAA/c_settings_fxaa.hpp
@@ -7,33 +7,40 @@
 #pragma once
 
 #if !PLATFORM_IS_DEDI
+
 #include <YeloLib/configuration/c_configuration_container.hpp>
 #include <YeloLib/configuration/c_configuration_value.hpp>
 #include <YeloLib/open_sauce/settings/c_settings_singleton.hpp>
 
 namespace Yelo
 {
-	namespace Rasterizer { namespace PostProcessing { namespace FXAA
-	{
-		class c_settings_container
-			: public Configuration::c_configuration_container
-		{
-		public:
-			Configuration::c_configuration_value<bool> m_enabled;
+    namespace Rasterizer
+    {
+        namespace PostProcessing
+        {
+            namespace FXAA
+            {
+                class c_settings_container
+                    : public Configuration::c_configuration_container
+                {
+                public:
+                    Configuration::c_configuration_value<bool> m_enabled;
 
-			c_settings_container();
+                    c_settings_container();
 
-		protected:
-			const std::vector<Configuration::i_configuration_value* const> GetMembers() final override;
-		};
+                protected:
+                    const std::vector<i_configuration_value* const> GetMembers() final override;
+                };
 
-		class c_settings_fxaa
-			: public Settings::c_settings_singleton<c_settings_container, c_settings_fxaa>
-		{
-		public:
-			void PostLoad() final override;
-			void PreSave() final override;
-		};
-	};};};
-};
+                class c_settings_fxaa
+                    : public Settings::c_settings_singleton<c_settings_container, c_settings_fxaa>
+                {
+                public:
+                    void PostLoad() final override;
+                    void PreSave() final override;
+                };
+            }
+        }
+    }
+}
 #endif
--- OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/FXAA/c_shader_fxaa.cpp
+++ OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/FXAA/c_shader_fxaa.cpp
@@ -6,46 +6,62 @@
 */
 #include "Common/Precompile.hpp"
 #include "Rasterizer/PostProcessing/FXAA/c_shader_fxaa.hpp"
+
 #if !PLATFORM_IS_DEDI
+
+#ifdef EXTERNAL_SUBSYSTEM_SHADERS
+#include "Rasterizer/PostProcessing/ShaderSources/c_shader_data_external.hpp"
+#else
+#include "Rasterizer/PostProcessing/ShaderSources/c_shader_data_pp_shaders.hpp"
+#endif
 
 namespace Yelo
 {
-	namespace Rasterizer { namespace PostProcessing { namespace FXAA
-	{
+    namespace Rasterizer
+    {
+        namespace PostProcessing
+        {
+            namespace FXAA
+            {
 #ifdef EXTERNAL_SUBSYSTEM_SHADERS
-
 #define SUBSYSTEM_FXAA
 #include "Rasterizer/PostProcessing/PostProcessing_ExternalSubsystemPaths.inl"
 #undef SUBSYSTEM_FXAA
-
 #endif
-		/////////////////////////////////////////////////
-		// shader setup
-		/*!
-		 * \brief
-		 * Sets up the shader classes shader data object.
-		 * 
-		 * Sets up the shader classes shader data object. When EXTERNAL_SUBSYSTEM_SHADERS is define the shader data object will be
-		 * a c_shader_data_external, otherwise it will be a c_shader_data_pp_shaders;
-		 * 
-		 * \see
-		 * c_shader_data_external | c_shader_data_pp_shaders
-		 */
-		void c_shader_fxaa::SetupShader()
-		{
-			YELO_ASSERT_DISPLAY(m_members_fxaa.shader_data == nullptr, "Shader data object has not been deleted and NULLed before being set again.");
+                void c_shader_fxaa::Ctor()
+                {
+                    c_shader_postprocess::Ctor();
+
+                    m_members_fxaa.shader_data = nullptr;
+                }
+
+                void c_shader_fxaa::Dtor()
+                {
+                    c_shader_postprocess::Dtor();
+
+                    m_members_fxaa.shader_data->Dtor();
+                    delete m_members_fxaa.shader_data;
+                    m_members_fxaa.shader_data = nullptr;
+                }
+
+                /// <summary>Sets up the shader.</summary>
+                void c_shader_fxaa::SetupShader()
+                {
+                    YELO_ASSERT_DISPLAY(m_members_fxaa.shader_data == nullptr, "Shader data object has not been deleted and NULLed before being set again.");
 
 #ifdef EXTERNAL_SUBSYSTEM_SHADERS
-			m_members_fxaa.shader_data = new c_shader_data_external();
-			m_members_fxaa.shader_data->Ctor();
-			m_members_fxaa.shader_data->SetFilePath(g_external_subsystem_shader_path, g_external_subsystem_include_path);
+                    m_members_fxaa.shader_data = new c_shader_data_external();
+                    m_members_fxaa.shader_data->Ctor();
+                    m_members_fxaa.shader_data->SetFilePath(g_external_subsystem_shader_path, g_external_subsystem_include_path);
 #else
-			m_members_fxaa.shader_data = new c_shader_data_pp_shaders();
-			m_members_fxaa.shader_data->Ctor();
-			m_members_fxaa.shader_data->SetShaderID("PP_FXAA");
+                    m_members_fxaa.shader_data = new c_shader_data_pp_shaders();
+                    m_members_fxaa.shader_data->Ctor();
+                    m_members_fxaa.shader_data->SetShaderID("PP_FXAA");
 #endif
-			SetSourceData(m_members_fxaa.shader_data);
-		}
-	}; }; };
-};
+                    SetSourceData(m_members_fxaa.shader_data);
+                }
+            }
+        }
+    }
+}
 #endif
