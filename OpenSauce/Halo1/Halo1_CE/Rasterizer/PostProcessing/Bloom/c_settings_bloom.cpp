1d0bbca471b779447d562f5f3337b809b08f42a5
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2015-10-11 17:51:13 +0100

    * Reformatted bloom classes

--- OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/Bloom/c_settings_bloom.cpp
+++ OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/Bloom/c_settings_bloom.cpp
@@ -8,31 +8,37 @@
 #include "Rasterizer/PostProcessing/Bloom/c_settings_bloom.hpp"
 
 #if !PLATFORM_IS_DEDI
+
 #include "Rasterizer/PostProcessing/Bloom/c_system_bloom.hpp"
 
 namespace Yelo
 {
-	namespace Rasterizer { namespace PostProcessing { namespace Bloom
-	{
-		c_settings_container::c_settings_container()
-			: Configuration::c_configuration_container("Rasterizer.PostProcessing.Bloom")
-			, m_enabled("Enabled", true)
-		{ }
+    namespace Rasterizer
+    {
+        namespace PostProcessing
+        {
+            namespace Bloom
+            {
+                c_settings_container::c_settings_container()
+                    : c_configuration_container("Rasterizer.PostProcessing.Bloom"),
+                      m_enabled("Enabled", true) { }
 
-		const std::vector<Configuration::i_configuration_value* const> c_settings_container::GetMembers()
-		{
-			return std::vector<i_configuration_value* const> { &m_enabled };
-		}
+                const std::vector<Configuration::i_configuration_value* const> c_settings_container::GetMembers()
+                {
+                    return std::vector<i_configuration_value* const> { &m_enabled };
+                }
 
-		void c_settings_bloom::PostLoad()
-		{
-			c_system_bloom::Instance().Enabled() = Get().m_enabled;
-		}
+                void c_settings_bloom::PostLoad()
+                {
+                    c_system_bloom::Instance().Enabled() = Get().m_enabled;
+                }
 
-		void c_settings_bloom::PreSave()
-		{
-			Get().m_enabled = c_system_bloom::Instance().Enabled();
-		}
-	};};};
+                void c_settings_bloom::PreSave()
+                {
+                    Get().m_enabled = c_system_bloom::Instance().Enabled();
+                }
+            };
+        };
+    };
 };
 #endif
