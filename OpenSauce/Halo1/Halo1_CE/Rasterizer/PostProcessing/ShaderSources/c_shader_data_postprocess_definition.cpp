d226095c61585f3493487ad3940edcd5d4a1c78d
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2015-10-14 21:11:49 +0100

    * Halo1_CE: Reformatted shader source classes.

--- OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/ShaderSources/c_shader_data_postprocess_definition.cpp
+++ OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/ShaderSources/c_shader_data_postprocess_definition.cpp
@@ -8,28 +8,73 @@
 #include "Rasterizer/PostProcessing/ShaderSources/c_shader_data_postprocess_definition.hpp"
 
 #if !PLATFORM_IS_DEDI
-#include <YeloLib/Halo1/shaders/shader_postprocess_definitions.hpp>
 
-#include "Rasterizer/PostProcessing/ShaderSources/c_shader_data_base.hpp"
+#include <YeloLib/Halo1/shaders/shader_postprocess_definitions.hpp>
 
 namespace Yelo
 {
-	namespace Rasterizer { namespace PostProcessing
-	{
-		const char* c_shader_data_postprocess_definition::DataSourceID()
-		{
-			uint32 name_length = strlen(m_shader_name) + 1;
-			if(name_length > k_data_source_id_max_length)
-				return &m_shader_name[name_length - k_data_source_id_max_length];
-			else
-				return m_shader_name;
-		}
+    namespace Rasterizer
+    {
+        namespace PostProcessing
+        {
+            void c_shader_data_postprocess_definition::ClearMembers()
+            {
+                // TODO: null m_shader_name?
 
-		const void* c_shader_data_postprocess_definition::GetData(uint32& data_size)
-		{
-			data_size = m_shader_definition->shader_code_binary.size;
-			return m_shader_definition->shader_code_binary.address;
-		}
-	};};
-};
+                m_shader_definition = nullptr;
+            }
+
+            void c_shader_data_postprocess_definition::SetShaderName(const char* shader_name)
+            {
+                m_shader_name = shader_name;
+            }
+
+            void c_shader_data_postprocess_definition::SetShaderDefinition(TagGroups::s_shader_postprocess_definition* definition)
+            {
+                m_shader_definition = definition;
+            }
+
+            void c_shader_data_postprocess_definition::Ctor()
+            {
+                ClearMembers();
+            }
+
+            void c_shader_data_postprocess_definition::Dtor()
+            {
+                ClearMembers();
+            }
+
+            const char* c_shader_data_postprocess_definition::DataSourceID()
+            {
+                uint32 name_length = strlen(m_shader_name) + 1;
+                if (name_length > k_data_source_id_max_length)
+                {
+                    return &m_shader_name[name_length - k_data_source_id_max_length];
+                }
+                return m_shader_name;
+            }
+
+            const void* c_shader_data_postprocess_definition::GetData(uint32& data_size)
+            {
+                data_size = m_shader_definition->shader_code_binary.size;
+                return m_shader_definition->shader_code_binary.address;
+            }
+
+            const bool c_shader_data_postprocess_definition::IsBinary()
+            {
+                return true;
+            }
+
+            const char* c_shader_data_postprocess_definition::GetIncludePath()
+            {
+                return nullptr;
+            }
+
+            const D3DXMACRO* c_shader_data_postprocess_definition::GetMacros()
+            {
+                return nullptr;
+            }
+        }
+    }
+}
 #endif
