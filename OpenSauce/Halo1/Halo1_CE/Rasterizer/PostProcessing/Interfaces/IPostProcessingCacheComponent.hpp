44949438f304cd0ef40b819de97978cb4e9b117c
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2015-10-14 20:48:17 +0100

    * Halo1_CE: Made postprocessing interfaces classes. * Halo1_CE: Reformatted postprocessing interfaces and interp class.

--- OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/Interfaces/IPostProcessingCacheComponent.hpp
+++ OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/Interfaces/IPostProcessingCacheComponent.hpp
@@ -5,18 +5,29 @@
 	See license\OpenSauce\Halo1_CE for specific license information
 */
 #pragma once
+
+#if !PLATFORM_IS_DEDI
+
 #include "Rasterizer/PostProcessing/Interfaces/IPostProcessingComponent.hpp"
 
 namespace Yelo
 {
-	namespace Rasterizer { namespace PostProcessing {
-		interface IPostProcessingCacheComponent : public IPostProcessingComponent
-		{
-			virtual void Initialize_Cache() {}
-			virtual void Dispose_Cache() {}
+    namespace Rasterizer
+    {
+        namespace PostProcessing
+        {
+            class IPostProcessingCacheComponent : public IPostProcessingComponent
+            {
+            public:
+                virtual void Initialize_Cache() {}
 
-			virtual void InitializeResources_Cache() {}
-			virtual void ReleaseResources_Cache() {}
-		};
-	};};
-};
+                virtual void Dispose_Cache() {}
+
+                virtual void InitializeResources_Cache() {}
+
+                virtual void ReleaseResources_Cache() {}
+            };
+        }
+    }
+}
+#endif
--- OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/Interfaces/IPostProcessingComponent.hpp
+++ OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/Interfaces/IPostProcessingComponent.hpp
@@ -10,27 +10,40 @@
 
 namespace Yelo
 {
-	namespace Rasterizer { namespace PostProcessing
-	{
-		interface IPostProcessingComponent
-			abstract
-		{
-	            virtual ~IPostProcessingComponent() {}
+    namespace Rasterizer
+    {
+        namespace PostProcessing
+        {
+            class IPostProcessingComponent
+            {
+            public:
+                virtual void Initialize() {}
 
-	            virtual void Initialize() {}
-			virtual void Dispose() {}
+                virtual void Dispose() {}
 
-			virtual void InitializeResources_Base(IDirect3DDevice9* device, D3DPRESENT_PARAMETERS* parameters) {}
-			virtual void OnLostDevice_Base() {}
-			virtual void OnResetDevice_Base(D3DPRESENT_PARAMETERS* parameters) {}
-			virtual void ReleaseResources_Base() {}
+                virtual void InitializeResources_Base(IDirect3DDevice9* device, D3DPRESENT_PARAMETERS* parameters) {}
 
-			virtual bool IsReady() { return false; }
-			virtual bool IsUnloaded() { return true; }
+                virtual void OnLostDevice_Base() {}
 
-			virtual void Unload() {}
-			virtual void Load() {}
-		};
-	};};
-};
+                virtual void OnResetDevice_Base(D3DPRESENT_PARAMETERS* parameters) {}
+
+                virtual void ReleaseResources_Base() {}
+
+                virtual bool IsReady()
+                {
+                    return false;
+                }
+
+                virtual bool IsUnloaded()
+                {
+                    return true;
+                }
+
+                virtual void Unload() {}
+
+                virtual void Load() {}
+            };
+        }
+    }
+}
 #endif
