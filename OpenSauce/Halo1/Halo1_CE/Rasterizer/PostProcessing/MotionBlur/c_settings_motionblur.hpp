23849a84650dc38731b9598cf048568b9c091b1a
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2015-10-14 21:03:35 +0100

    * Halo1_CE: Reformatted motion blur classes.

--- OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/MotionBlur/c_settings_motionblur.hpp
+++ OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/MotionBlur/c_settings_motionblur.hpp
@@ -7,34 +7,41 @@
 #pragma once
 
 #if !PLATFORM_IS_DEDI
+
 #include <YeloLib/configuration/c_configuration_container.hpp>
 #include <YeloLib/configuration/c_configuration_value.hpp>
 #include <YeloLib/open_sauce/settings/c_settings_singleton.hpp>
 
 namespace Yelo
 {
-	namespace Rasterizer { namespace PostProcessing { namespace MotionBlur
-	{
-		class c_settings_container
-			: public Configuration::c_configuration_container
-		{
-		public:
-			Configuration::c_configuration_value<bool> m_enabled;
-			Configuration::c_configuration_value<real> m_blur_amount;
+    namespace Rasterizer
+    {
+        namespace PostProcessing
+        {
+            namespace MotionBlur
+            {
+                class c_settings_container
+                    : public Configuration::c_configuration_container
+                {
+                public:
+                    Configuration::c_configuration_value<bool> m_enabled;
+                    Configuration::c_configuration_value<real> m_blur_amount;
 
-			c_settings_container();
+                    c_settings_container();
 
-		protected:
-			const std::vector<i_configuration_value* const> GetMembers() final override;
-		};
+                protected:
+                    const std::vector<i_configuration_value* const> GetMembers() final override;
+                };
 
-		class c_settings_motionblur
-			: public Settings::c_settings_singleton<c_settings_container, c_settings_motionblur>
-		{
-		public:
-			void PostLoad() final override;
-			void PreSave() final override;
-		};
-	};};};
-};
+                class c_settings_motionblur
+                    : public Settings::c_settings_singleton<c_settings_container, c_settings_motionblur>
+                {
+                public:
+                    void PostLoad() final override;
+                    void PreSave() final override;
+                };
+            }
+        }
+    }
+}
 #endif
