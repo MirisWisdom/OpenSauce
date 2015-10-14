dc6ff63016f0a831ec8264917e7aa031afd62b97
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2015-10-14 21:11:49 +0100

    * Halo1_CE: Reformatted shader source classes.

--- OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/ShaderSources/c_shader_data_external.cpp
+++ OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/ShaderSources/c_shader_data_external.cpp
@@ -8,66 +8,102 @@
 #include "Rasterizer/PostProcessing/ShaderSources/c_shader_data_external.hpp"
 
 #if !PLATFORM_IS_DEDI
-#include <YeloLib/Halo1/shaders/shader_postprocess_definitions.hpp>
-#include "Rasterizer/PostProcessing/ShaderSources/c_shader_data_base.hpp"
+
+#include "Common/FileIO.hpp"
 
 namespace Yelo
 {
-	namespace Rasterizer { namespace PostProcessing
-	{
-		const char* c_shader_data_external::DataSourceID()
-		{
-			uint32 id_length = strlen(m_file_path) + 1;
-			if(id_length > k_data_source_id_max_length)
-				return &m_file_path[id_length - k_data_source_id_max_length];
-			else
-				return m_file_path;
-		}
+    namespace Rasterizer
+    {
+        namespace PostProcessing
+        {
+            const char* c_shader_data_external::DataSourceID()
+            {
+                uint32 id_length = strlen(m_file_path) + 1;
+                if (id_length > k_data_source_id_max_length)
+                {
+                    return &m_file_path[id_length - k_data_source_id_max_length];
+                }
+                return m_file_path;
+            }
 
-		void c_shader_data_external::SetFilePath(const char* file_path, const char* include_path)
-		{
-			m_file_path = file_path;
-			m_include_path = include_path;
-		}
+            void c_shader_data_external::SetFilePath(const char* file_path, const char* include_path)
+            {
+                m_file_path = file_path;
+                m_include_path = include_path;
+            }
 
-		const void* c_shader_data_external::GetData(uint32& data_size)
-		{
-			FileIO::s_file_info file_info;
-			bool success = false;
-			do
-			{
-				if(FileIO::OpenFile(file_info, m_file_path))
-					break;
-				if(FileIO::ReadFileToMemory(file_info, CAST_PTR(void**,&m_file_data)))
-					break;
-				data_size = file_info.file_size;
+            void c_shader_data_external::Ctor()
+            {
+                m_file_data = nullptr;
+                m_file_path = nullptr;
+                m_include_path = nullptr;
 
-				success = true;
-			}while(false);
+                m_macros[0].Name = "SHADER_EXTERNAL";
+                m_macros[0].Definition = nullptr;
+                m_macros[1].Name = nullptr;
+                m_macros[1].Definition = nullptr;
+            }
 
-			FileIO::CloseFile(file_info);
+            void c_shader_data_external::Dtor()
+            {
+                m_file_path = nullptr;
+                m_include_path = nullptr;
 
-			if(!success)
-				return NULL;
+                delete [] m_file_data;
+                m_file_data = nullptr;
+            }
 
-			return m_file_data;
-		}
+            const void* c_shader_data_external::GetData(uint32& data_size)
+            {
+                FileIO::s_file_info file_info;
+                auto success = false;
+                do
+                {
+                    if (FileIO::OpenFile(file_info, m_file_path))
+                    {
+                        break;
+                    }
+                    if (ReadFileToMemory(file_info, CAST_PTR(void**,&m_file_data)))
+                    {
+                        break;
+                    }
+                    data_size = file_info.file_size;
 
-		void c_shader_data_external::Dispose()
-		{
-			delete [] m_file_data;
-			m_file_data = NULL;
-		}
+                    success = true;
+                } while (false);
 
-		const char* c_shader_data_external::GetIncludePath()
-		{
-			return m_include_path;
-		}
+                CloseFile(file_info);
 
-		const D3DXMACRO* c_shader_data_external::GetMacros()
-		{
-			return &m_macros[0];
-		}
-	};};
-};
+                if (!success)
+                {
+                    return nullptr;
+                }
+
+                return m_file_data;
+            }
+
+            void c_shader_data_external::Dispose()
+            {
+                delete [] m_file_data;
+                m_file_data = nullptr;
+            }
+
+            const bool c_shader_data_external::IsBinary()
+            {
+                return false;
+            }
+
+            const char* c_shader_data_external::GetIncludePath()
+            {
+                return m_include_path;
+            }
+
+            const D3DXMACRO* c_shader_data_external::GetMacros()
+            {
+                return &m_macros[0];
+            }
+        }
+    }
+}
 #endif
--- OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/ShaderSources/c_shader_data_pp_shaders.cpp
+++ OpenSauce/Halo1/Halo1_CE/Rasterizer/PostProcessing/ShaderSources/c_shader_data_pp_shaders.cpp
@@ -8,22 +8,61 @@
 #include "Rasterizer/PostProcessing/ShaderSources/c_shader_data_pp_shaders.hpp"
 
 #if !PLATFORM_IS_DEDI
+
 #include "Rasterizer/PostProcessing/c_post_processing_main.hpp"
 
 namespace Yelo
 {
-	namespace Rasterizer { namespace PostProcessing
-	{
-		const char* c_shader_data_pp_shaders::DataSourceID()
-		{
-			return m_shader_id;
-		}
+    namespace Rasterizer
+    {
+        namespace PostProcessing
+        {
+            void c_shader_data_pp_shaders::ClearMembers()
+            {
+                m_shader_id[0] = '\0';
+            }
 
-		const void* c_shader_data_pp_shaders::GetData(uint32& data_size)
-		{
-			data_size = 0;
-			return c_post_processing_main::Instance().Globals().shader_file.GetDataPointer(&m_shader_id[0], &data_size);
-		}
-	};};
-};
+            void c_shader_data_pp_shaders::SetShaderID(const char* id)
+            {
+                strcpy_s(m_shader_id, MAX_PATH, id);
+            }
+
+            void c_shader_data_pp_shaders::Ctor()
+            {
+                ClearMembers();
+            }
+
+            void c_shader_data_pp_shaders::Dtor()
+            {
+                ClearMembers();
+            }
+
+            const char* c_shader_data_pp_shaders::DataSourceID()
+            {
+                return m_shader_id;
+            }
+
+            const void* c_shader_data_pp_shaders::GetData(uint32& data_size)
+            {
+                data_size = 0;
+                return c_post_processing_main::Instance().Globals().shader_file.GetDataPointer(&m_shader_id[0], &data_size);
+            }
+
+            const bool c_shader_data_pp_shaders::IsBinary()
+            {
+                return true;
+            }
+
+            const char* c_shader_data_pp_shaders::GetIncludePath()
+            {
+                return nullptr;
+            }
+
+            const D3DXMACRO* c_shader_data_pp_shaders::GetMacros()
+            {
+                return nullptr;
+            }
+        }
+    }
+}
 #endif
