537f878d2d2325d8086964da20b796fe73085199
David Wilson <none@none>
2016-12-18 05:09:53 -0500

    * Halo1_General: Syncing the database to system restore changes.

--- OpenSauce/Halo1/Halo1_CE/Objects/ObjectsYelo.cpp
+++ OpenSauce/Halo1/Halo1_CE/Objects/ObjectsYelo.cpp
@@ -161,6 +161,8 @@ namespace Yelo
 				scripting_unit_weapon_set_position_evaluate);
 			Scripting::InitializeScriptFunctionWithParams(Enums::_hs_function_unit_is_key_down,
 				scripting_unit_is_key_down_evaluate);
+			Scripting::InitializeScriptFunction(Enums::_hs_function_camera_fov_get,
+				scripting_camera_fov_get_evaluate);
 		}
 		
 		bool PLATFORM_API ShouldKillChildObject(const s_object_datum* object_datum)
