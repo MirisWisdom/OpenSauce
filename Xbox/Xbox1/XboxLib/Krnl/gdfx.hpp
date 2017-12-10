a3120469fcd4ce4aea0340ef8fec7ad427439551
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2017-12-10 19:54:07 +0000

    * Removed unused projects

--- Xbox/Xbox1/XboxLib/Krnl/gdfx.hpp
+++
@@ -1,6 +0,0 @@
-/*
-	OpenSauceBox: SDK for Xbox User Modding
-
-	See license\Xbox\Xbox for specific license information
-*/
-#pragma once
--- Xbox/Xenon/HaloWars_ECF360/Precompile.cpp
+++
@@ -1,8 +0,0 @@
-/*
-	OpenSauce: HaloWars Data Interop
-
-	See license\OpenSauce\OpenSauce for specific license information
-*/
-#include "Precompile.hpp"
-
-c_basic_log_writer g_log_writer;
