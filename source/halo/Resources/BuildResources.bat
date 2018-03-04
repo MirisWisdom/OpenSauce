43c13d2e5853a961080ccd9f8c8ad99682536884
Martin Ballantyne <TheFieryScythe@users.noreply.github.com>
2018-03-04 11:10:40 +0000

    * The great movening, restructuring codebase for automated project creation

---
+++ source/halo/Resources/BuildResources.bat
@@ -0,0 +1,4 @@
+cd "D:\Program Files (x86)\Microsoft Games\Halo Custom Edition"
+os_tool build-packed-file "D:\Gamedev\opensauce\OpenSauce\Halo1\Halo1_CE\Resources\OpenSauceUI" "D:\Gamedev\opensauce\bin\Resources\Halo1_CE" Halo1_CE_OpenSauceUI.xml
+copy "D:\Gamedev\opensauce\bin\Resources\Halo1_CE\OpenSauceUI.pak" "%ProgramData%\Kornner Studios\Halo CE\OpenSauceUI.pak"
+pause
